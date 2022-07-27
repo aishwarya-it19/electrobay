da006d43a160957117454e8572f6a95c8814a001
aishwarya <aishwarya.it19@bitsathy.ac.in>
2022-07-27 19:37:30 +0530

    commit

---
+++ src/main/java/com/example/electrobay/entity/Cart.java
@@ -0,0 +1,45 @@
+package com.example.electrobay.entity;
+
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "cart")
+public class Cart {
+
+//    private int id;
+    protected int userId;
+    private int productId;
+    private int quantity;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+
+}
